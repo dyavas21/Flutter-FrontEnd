88d5433c99db725fd96ec50ed864321212f86817
Gede Dyava S <dyavas21@gmail.com>
2021-10-22 15:49:18 +0700

    new folder
    beda design

---
+++ Exercise/travel_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(TravelApp());
+
+class TravelApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => Menu(),
+      },
+    );
+  }
+}
